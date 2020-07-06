85c4dc71dff3c38e65c219c26f271a5f9866e673
Yiming Wei <weiyiming20@gmail.com>
2020-07-06 17:28:37 +0800

    Update RunningSum.java
    add RunningSum

--- src/RunningSum/RunningSum.java
+++ src/RunningSum/RunningSum.java
@@ -1,5 +1,14 @@
 package RunningSum;
 
 public class RunningSum {
-
+	public int[] runningSum(int[] nums) {
+        int sum = 0;
+        int[] result = new int[nums.length];
+        for(int i = 0; i < nums.length; i++)
+        {
+            sum += nums[i];
+            result[i] = sum;
+        }
+        return result;
+    }
 }
